68917013eadeb3b01c97fd814578fb2d247fb4ad
BeeBombshell <vbhavya269@gmail.com>
2023-02-25 19:31:17 +0530

    ✨ Added Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Hero({}: Props) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About Me */}
 
