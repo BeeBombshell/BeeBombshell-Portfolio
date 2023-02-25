11a78936cca65e430a4f2ba7853df594aa6f4304
BeeBombshell <vbhavya269@gmail.com>
2023-02-25 19:43:31 +0530

    ✨ Added typewriter effect to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 type Props = {}
 
-function Hero({}: Props) {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+export default function Hero({}: Props) {
+    
+    const [text, count] = useTypewriter({
+        words: [
+            "Hey There, I'm Bhavya Verma",
+            "Aka BeeBombshell",
+            "A Software Engineer",
+            "Open Source Enthusiast",
+        ],
+        loop: true, 
+        delaySpeed: 2000,
+    })
 
-export default Hero
+    return (
+        <div>
+            <h1>
+                <span>{text}</span>
+                <Cursor cursorColor='#00C1CB' />
+            </h1>
+        </div>
+    )
+}
