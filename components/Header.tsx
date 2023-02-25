af8e5e1a6f7567084c4cd4a1022bd718157ca6db
BeeBombshell <vbhavya269@gmail.com>
2023-02-25 18:08:31 +0530

    ⚡️  Updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto'>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <div className='flex flex-row items-center'>
             {/* Social icons */}
             <SocialIcon 
