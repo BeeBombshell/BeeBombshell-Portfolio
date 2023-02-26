6d1f94f8f9862c4cff2a958e3910300e77c9c84b
BeeBombshell <vbhavya269@gmail.com>
2023-02-26 13:55:27 +0530

    ⚡️ Added hero asset + UI

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -20,10 +21,23 @@ export default function Hero({}: Props) {
     return (
         <div className='h-screen flex flex-col space-y-0 items-center justify-center text-center overflow-hidden'>
             <BackgroundCircles />
-            <h1>
-                <span>{text}</span>
-                <Cursor cursorColor='#00C1CB' />
-            </h1>
+            <Image 
+                className='relative rounded-full h-32 w-32 mx-auto'
+                src='/../public/beebombshell.png'
+                alt='Bee'
+                width={300}
+                height={300}
+            />
+            <div>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+                    Software Engineer
+                </h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor='#00C1CB' />
+                </h1>
+
+            </div>
         </div>
     )
 }
