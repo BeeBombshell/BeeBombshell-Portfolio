6da86e678c643f15e95a55ffbd1dab653bfb8f69
BeeBombshell <vbhavya269@gmail.com>
2023-08-08 01:21:10 +0530

    ✨ Added About Section

---
+++ components/About.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+export default function About({}: Props) {
+  return (
+    <motion.div 
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1}}
+    transition={{ duration: 1.5 }}
+
+    className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        About
+      </h3>
+
+      <motion.img
+        initial={{ 
+          x:-200,
+          opacity:0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{
+          x:0,
+          opacity:1,
+        }}
+        viewport={{ once: true }}
+        src='/bhavya.jpeg'
+        width={500}
+        className='-mb-20 md:mb-0 flex-shrink-0 w-59 h-59 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[500px]'
+      />
+
+      <div className='space-y-10 px-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>
+          Here's a <span className='underline decoration-purple-500'>little</span> background
+        </h4>
+        <p className='text-base'>
+        Hey there! 👋 I'm Bhavya Verma, a Software Engineer driving innovation at Rowy, a commercial open-source startup. 
+        With a strong passion for open source, I've contributed to diverse organizations, mentored budding talents at the Grace Hopper Celebration Open Source Day, and personally guided contributors through open source programs. 💜 I'm an advocate for diversity, writing for AnitaBorg and Women Who Code. Sharing my journey and insights on Twitter and personal blog. Let's craft a more inclusive tech future together! 🚀
+        </p>
+      </div>
+    </motion.div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 
 export default function Home() {
@@ -16,11 +17,14 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
 
       {/* About Me */}
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
 
