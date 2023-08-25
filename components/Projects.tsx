b8c0b5d6bd7febfe99505864a2cc8a5473007dae
BeeBombshell <vbhavya269@gmail.com>
2023-08-26 01:37:19 +0530

    💚 Fixed CI Build Issue

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,24 +4,25 @@ import { motion } from 'framer-motion'
 type Props = {}
 
 function Projects({ }: Props) {
-  const projects = [1, 2, 3, 4, 5, 6, 7, 8]
+  const projects = [1, 2, 3, 4, 5]
   return (
     <motion.div 
     initial= {{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     transition={{ duration: 1.5 }}
     className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Projects
       </h3>
 
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects.map((project, i) => (
-          <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+          <div 
+          className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'
+          key={i}>
             <motion.img
               initial = {{
-                y: -300,
+                y: -300, 
                 opacity: 0
               }}
               transition = {{ duration: 1.2 }}
@@ -38,7 +39,7 @@ function Projects({ }: Props) {
               </h4>
 
               <p className='text-lg text-center md:text-left'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
               </p>
             </div>
           </div>
